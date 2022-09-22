4af9a8e99c74c134d46ad50d3daa32ed2a57bd96
ELIGIANI DA SILVA MONTEIRO <01962289133@ifms.edu.br>
2022-09-22 19:40:41 -0400

    Projeto Inciado

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,39 @@
+function Formulario() {
+    return(
+        <div className="formulario">
+
+            <label>Nome</label>
+            <input type ="text"></input>
+
+            <label>Rg</label>
+            <input type ="text"></input>
+
+            <label>Cpf</label>
+            <input type ="text"></input>
+
+            <label>Data Nacimento</label>
+            <input type="date"></input>
+
+            <label>Cp</label>
+            <input type="text"></input>
+
+            <label>Endereco</label>
+            <input type ="text"></input>
+
+            <label>Cidade</label>
+            <input type ="text"></input>
+
+            <label>Estado</label>
+            <input type ="text"></input>
+            <button>Salvar</button>
+            
+            
+
+            
+
+
+        </div>
+    )
+    
+}
+export default Formulario
